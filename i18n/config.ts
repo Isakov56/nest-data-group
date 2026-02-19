b7176586b5d3e990bb0d2c1fe30c1c5808a6995f
Khojiakbarkhon <isakov56cc@gmail.com>
2026-02-19 16:49:51 +0500

    en ja ru

--- i18n/config.ts
+++ i18n/config.ts
@@ -1,4 +1,4 @@
-export const locales = ['en', 'ru', 'ja'] as const;
+export const locales = ['en', 'ja', 'ru'] as const;
 export type Locale = (typeof locales)[number];
 export const defaultLocale: Locale = 'en';
 
